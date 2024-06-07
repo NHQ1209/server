0960d3dc4501e2b4eceff6e5c293342eef4ad6f2
Quân Nguyễn <nguyenqwn@gmail.com>
2024-06-08 00:32:57 +0700

    web

---
+++ src/services/boardService.js
@@ -0,0 +1,91 @@
+
+import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
+import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
+
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
+
+const createNew = async (reqBody) => {
+  try {
+    // Xử lý logic dữ liệu tùy đặc thù dự án
+    const newBoard = {
+      ...reqBody,
+      slug: slugify(reqBody.title)
+    }
+
+    // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
+  
+    return getNewBoard
+  } catch (error) { throw error }
+}
+
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+
+    
+    const resBoard = cloneDeep(board)
+
+    // B2: Đưa card về đúng column của nó
+    resBoard.columns.forEach(column => {
+      // Cách dùng .equals này là bởi vì chúng ta hiểu ObjectId trong MongoDB có support method .equals
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+
+    
+    })
+
+ 
+    delete resBoard.cards
+
+    return resBoard
+  } catch (error) { throw error }
+}
+
+const update = async (boardId, reqBody) => {
+  try {
+    const updateData = {
+      ...reqBody,
+      updatedAt: Date.now()
+    }
+    const updatedBoard = await boardModel.update(boardId, updateData)
+
+    return updatedBoard
+  } catch (error) { throw error }
+}
+
+const moveCardToDifferentColumn = async (reqBody) => {
+  try {
+    
+    await columnModel.update(reqBody.prevColumnId, {
+      cardOrderIds: reqBody.prevCardOrderIds,
+      updatedAt: Date.now()
+    })
+    await columnModel.update(reqBody.nextColumnId, {
+      cardOrderIds: reqBody.nextCardOrderIds,
+      updatedAt: Date.now()
+    })
+    await cardModel.update(reqBody.currentCardId, {
+      columnId: reqBody.nextColumnId
+    })
+
+    return { updateResult: 'Successfully!' }
+  } catch (error) { throw error }
+}
+
+export const boardService = {
+  createNew,
+  getDetails,
+  update,
+  moveCardToDifferentColumn
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+export const WHITELIST_DOMAINS = [
+ 
+]
+
+export const BOARD_TYPES = {
+  PUBLIC: 'public',
+  PRIVATE: 'private'
+}
